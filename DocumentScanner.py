aa3eaf226265ecd0965bc8cc148030530aa60672
Ben <benw@student.chalmers.se>
2020-12-22 21:13:55 +0100

    Threaded camera, More crop options, Faster video preview, Batches

--- DocumentScanner.py
+++ DocumentScanner.py
@@ -5,16 +5,19 @@ Start running the program.
 * Enter SPACEBAR to capture images. 
 * Enter ESC to stop capturing
 * Program will now display the images captured one by one.
-* Press SPACEBAR to mark the edges of the page for cropping
+* Mark the corners. (Only 4). Right click to start over. Q to not crop. D to delete.
 * Press ESC after marking. Repeat the same for all the images captured.
-* Once finished the images captured, their scanned verstions and a PDF file will be visible on the same folder where the code is saved, 
+* Once finished, the cropped versions and will be in the same folder the code is saved in
 """
 
+import os
 import numpy
 import cv2 as cv2
 from urllib.request import urlopen
+import configparser
 import imutils
 from PIL import Image
+from threading import Thread
 
 refPt = []
 width_img = 0
@@ -26,6 +29,30 @@ p_url = 'http://192.168.86.183:8080/photo.jpg'
 v_url = 'http://192.168.86.183:8080/video'
 
 mark_str = "MARK THE CORNERS"
+
+
+class ThreadedCamera(object):
+    def __init__(self, source=""):
+        print("Connecting to video feed...")
+        self.capture = cv2.VideoCapture(source)
+        print("Connection successful.")
+
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
+
+        self.status = False
+        self.frame = None
+
+    def update(self):
+        while True:
+            if self.capture.isOpened():
+                (self.status, self.frame) = self.capture.read()
+
+    def grab_frame(self):
+        if self.status:
+            return self.frame
+        return None
 
 
 def display_im(name, im):
@@ -54,20 +81,29 @@ def click_event(event, x, y, flags, param):
 def crop(original, im):
     global refPt, img
     display_im(mark_str, org)
-    print("Mark the corners. (Only 4). Right click to start over. Q to not crop. Press escape when done. ")
+    print("Mark the corners. (Only 4). Right click to start over. Q to not crop. D to delete. Press escape when done. ")
     while True:
         cv2.setMouseCallback(mark_str, click_event)
-        x = cv2.waitKey(0)
-        # escape key
-        if x == 27:
+        kc = cv2.waitKey(0)
+        # Escape key, attempt crop
+        if kc == 27:
             pass
-        # Q Key
-        elif x == 113:
+        # Q Key, Don't crop
+        elif kc == 113:
             print(original)
             return original.copy()
+        # D Key, delete photo
+        elif kc == 100:
+            print("Press E to confirm.")
+            kc2 = cv2.waitKey(0)
+            # Not E Key, Cancel deletion
+            if kc2 != 101:
+                continue
+            return None
+
         else:
             continue
-        print(refPt)
+        # print(refPt)
         cv2.destroyAllWindows()
         photo_contour = org.copy()
 
@@ -87,9 +123,10 @@ def crop(original, im):
         photo_cropped = cv2.warpPerspective(original, matrix, (width_img, height_img))
         return photo_cropped
 
+
 # function that saves the scanned image to the folder
-def save_scanned(cropped, count):
-    img_name = "Cropped_Photo_{}.png".format(count)
+def save_cropped(cropped, count):
+    img_name = "Photo_{}_{}.png".format(batch, count)
     cv2.imwrite(img_name, cropped)
     print("{} written!".format(img_name))
     count += 1
@@ -97,11 +134,12 @@ def save_scanned(cropped, count):
 
 
 # function that saves the image from the webcam to the folder
-def capture(img_counter):
-    global width_img, height_img, resize_ratio
-    img_name = "test_photo_{}.png".format(img_counter)
-
+def capture():
     req = urlopen(p_url)
+
+    global width_img, height_img, resize_ratio
+    img_name = "raw/image_{}_{}.png".format(batch, conf["config"]["count"])
+
     arr = numpy.asarray(bytearray(req.read()), dtype=numpy.uint8)
     photo = cv2.imdecode(arr, -1)  # 'Load it as it is'
 
@@ -113,34 +151,45 @@ def capture(img_counter):
     resize_ratio = 1 / (photo.shape[0] / 640)
 
     display_im("Latest Saved Image", photo)
-    img_counter += 1
-    return img_name, img_counter
+    conf["config"]["count"] = str(int(conf["config"]["count"]) + 1)
+    return img_name
+
+
+def save_ini():
+    with open("conf.ini", "w+") as f:
+        conf.write(f)
 
 
 # main function
 if __name__ == "__main__":
-    # enter ip here
-    print("Connecting to video feed...")
-    cap = cv2.VideoCapture(v_url)
-    print("Connection successful.")
-    image_list = []
-    scan_list = []
-    img_counter = 0
-    while True:
-        ret, frame = cap.read()
+    if not os.path.exists('raw'):
+        os.makedirs('raw')
 
-        video = cv2.resize(frame, (480, 640))
-        cv2.imshow("Camera Preview", video)
+    # enter ip here
+    conf = configparser.ConfigParser()
+    conf.read("conf.ini")
+    batch = conf["config"]["batch"]
+
+    streamer = ThreadedCamera(v_url)
+    # cap = cv2.VideoCapture(v_url)
+
+    image_list = []
+
+    while True:
+        frame = streamer.grab_frame()
+        if frame is not None:
+            video = cv2.resize(frame, (480, 640))
+            cv2.imshow("Camera Preview", video)
 
         q = cv2.waitKey(1)
 
         # Press SPACE to capture
         if q == 32:
-            image_name, img_counter = capture(img_counter)
+            image_name = capture()
 
             image_list.append(image_name)
 
-            # Press ESC to Scan the captured images
+        # Press ESC to Crop the captured images
         if q == 27:
             cv2.destroyAllWindows()
             count = 0
@@ -151,6 +200,11 @@ if __name__ == "__main__":
 
                 org = img.copy()
                 img = crop(org, img)
-                scanName, count = save_scanned(img, count)
-                scan_list.append(scanName)
+                if img is not None:
+                    cropName, count = save_cropped(img, count)
+
             break
+
+    conf["config"]["batch"] = str(int(conf["config"]["batch"]) + 1)
+    conf["config"]["count"] = "0"
+    save_ini()
